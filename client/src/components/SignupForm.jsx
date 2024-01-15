b4a73408b926809b1289ee40be178670a846ea1f
clayg <claytondguerrero@gmail.com>
2024-01-15 13:56:33 -0600

    edited LOGIN_USER

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Alert, Button, Form } from "react-bootstrap";
+import { Form, Button, Alert } from "react-bootstrap";
 
 import { useMutation } from "@apollo/client";
 import Auth from "../utils/auth";
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,7 +1,7 @@
 import { gql } from "@apollo/client";
 
 export const LOGIN_USER = gql`
-  mutation login($email: Sting!, $password: String!) {
+  mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
       user {
@@ -11,7 +11,6 @@ export const LOGIN_USER = gql`
     }
   }
 `;
-
 export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
