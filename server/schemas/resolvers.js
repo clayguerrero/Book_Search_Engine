cc0a3991409be0a3ee9996ad8ec6dfc4122ea874
clayg <claytondguerrero@gmail.com>
2024-01-14 19:15:53 -0600

    edited various files

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,13 +3,6 @@ const { signToken, AuthenticationError } = require("../utils/auth");
 
 const resolvers = {
   Query: {
-    users: async () => {
-      return User.find();
-    },
-    user: async (parent, { userId }) => {
-      return User.find({ _id: userId });
-    },
-
     me: async (parent, args, context) => {
       if (context.user) {
         return User.findOne({ _id: context.user._id });
